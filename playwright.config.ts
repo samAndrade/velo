83d5e513c2aaac16d83b2e3357e01deefb906759
Samir Andrade <samir.andrade26@gmail.com>
2026-08-05 21:19:48 -0300

    build e deploy no vercel com secret de token correta

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: 'https://velo-samandrade.vercel.app',
+    baseURL: 'https://velo-eleu8mgop-samir-andrade.vercel.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',  //'retain-on-failure', //'on-first-retry',
