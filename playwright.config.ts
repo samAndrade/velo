127c884d4201c5b7f2563ac6820d00a8ae7d1b96
Samir Andrade <samir.andrade26@gmail.com>
2026-08-12 20:55:08 -0300

    estágios de execução dos testes de regressão

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: 'https://velo-eleu8mgop-samir-andrade.vercel.app',
+    baseURL: process.env.BASE_URL || 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',  //'retain-on-failure', //'on-first-retry',
